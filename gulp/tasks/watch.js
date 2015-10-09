42a333ebe7d8fc1cf2f8443eb8ab5d7160b0afde
Carlos Cruz <cruz5114@gmail.com>
2015-10-09 14:08:57 -0600

    update gulp process to watch html and css

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 
 gulp.task('watch', ['setWatch', 'browserSync'], function() {
-  gulp.watch('src/css/**', ['css']);
+  gulp.watch('src/style/**', ['css']);
   gulp.watch(['src/images/**', '!src/images/map/**'], ['images']);
-  gulp.watch( ['src/htdocs/*.html','src/htdocs/*.json'], ['markup-watch']);
+  gulp.watch( ['src/html/*.html','src/html/*.json'], ['markup-watch']);
 });
